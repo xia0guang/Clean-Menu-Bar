e08fe6414a14078ce4908fbdfb1de8858f63d62e
xia0guang <wu890120@gmail.com>
2021-05-03 17:44:29 -0700

    Initial Commit

---
+++ Clean Menu Bar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Clean Menu Bar
+//
+//  Created by Ray Wu on 5/3/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
